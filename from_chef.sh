8b7abed5adce9e8f533266dd42bc928a928b1805
Kale Kundert <kale@thekunderts.net>
2018-06-26 15:20:47 -0700

    Update sync scripts, now that this is a git repo.

--- from_chef.sh
+++ from_chef.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SRC=ksi/notebook/20180604_benchmark_the_buried_unsatisfied_h_bond_penalty
+#SRC=ksi/notebook/20180604_benchmark_the_buried_unsatisfied_h_bond_penalty
+SRC=ksi/20180219_res16_loop12_lhkic_del1
 rsync \
-    chef:$SRC/ \
-    . \
+    chef:$SRC/park2016/outputs/ \
+    park2016/outputs/ \
     --archive \
     --recursive \
     --verbose \
