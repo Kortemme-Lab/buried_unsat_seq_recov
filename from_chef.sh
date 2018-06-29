77d1f17e13a69d482dd7ed2e3ba444cc14c5e15b
Kale Kundert <kale@thekunderts.net>
2018-06-29 16:09:26 -0700

    Correct the remote path.

--- from_chef.sh
+++ from_chef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#SRC=ksi/notebook/20180604_benchmark_the_buried_unsatisfied_h_bond_penalty
-SRC=ksi/20180219_res16_loop12_lhkic_del1
+SRC=ksi/notebook/20180604_benchmark_the_buried_unsatisfied_h_bond_penalty
 rsync \
     chef:$SRC/park2016/outputs/ \
     park2016/outputs/ \
     --archive \
     --recursive \
     --verbose \
+    --exclude 'pdb/' \
 
