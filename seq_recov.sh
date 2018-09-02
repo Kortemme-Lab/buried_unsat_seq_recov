fc74284b6034956c8f3160be5f63f9af5e01cfc6
Kale Kundert <kale@thekunderts.net>
2018-09-01 20:33:07 -0700

    Update the sequence recovery scripts.

--- seq_recov.sh
+++ seq_recov.sh
@@ -7,7 +7,7 @@ source helpers.sh
 #$ -l mem_free=1G                  #-- submits on nodes with enough free memory (required)
 #$ -l arch=linux-x64               #-- SGE resources (CPU type)
 #$ -l netapp=1G,scratch=1G         #-- SGE resources (home and scratch disks)
-#$ -l h_rt=24:00:00                #-- runtime limit (see above; this requests 24 hours)
+#$ -l h_rt=48:00:00                #-- runtime limit (see above; this requests 24 hours)
 
 if [ "$PDB" = "" ]; then
     echo "Usage: PDB=... $0"
@@ -15,15 +15,9 @@ if [ "$PDB" = "" ]; then
     exit 1
 fi
 
-# Using the unrelaxed structures, because relaxing seems to move the structures 
-# a lot, and may make the native recovery benchmark too easy.
-
 $BIN/buried_unsats.$ROSETTA_BUILD                                             \
-    -in:file:s $INPUTS/pdb/orig/$PDB.pdb                                      \
-    -packing:resfile $INPUTS/resfile/$PDB.resfile                             \
-    -app:resis 303 \
+    -in:file:s $INPUTS/pdb/$SFXN/$PDB.pdb                                     \
     -app:sfxn $SFXN                                                           \
     -app:out:scores $OUTPUTS/$SFXN/score/$PDB.scores                          \
     -app:out:pdbs $OUTPUTS/$SFXN/pdb/${PDB}_                                  \
     -app:out:save_pdbs $DUMP_PDB                                              \
-    
